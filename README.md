7fc4e5553d6dcce7645e3cd7290dc3ce358a2481
D3Portillo <d3portillo@gmail.com>
2025-12-02 18:45:46 +0100

    chores: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Degen Conch
+
+Ask the Conch anything! Is ETH going up? Is MON mooning? The Conch knows all.
---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import { Fragment } from "react/jsx-runtime"
+
+export default function Home() {
+  return (
+    <Fragment>
+      <main className="p-4 max-w-md mx-auto">
+        <h2 className="text-xl font-semibold mb-3">
+          Is Baseposting bad for my health?
+        </h2>
+
+        <img
+          alt=""
+          className="w-full pointer-events-none"
+          src="https://media4.giphy.com/media/v1.Y2lkPTZjMDliOTUyZXBoMTdwYnB0cnZreHpsbWFhZW5yanZmOWw3MWI2OGVsazI3cmxmOSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/NcsEoyGjuLUYg/source.gif"
+        />
+      </main>
+    </Fragment>
+  )
+}
